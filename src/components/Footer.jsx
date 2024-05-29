52f17e398ee86987b316e06b6610252dd3543142
imranhwebdev <alimrancv870@gmail.com>
2024-05-29 13:11:17 +0600

    all done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,11 +19,11 @@ export default function Footer(){
           },
           {
             icon:<img src={socialIcon3} alt="Social Icon" />,
-            url: "https://www.google.com",
+            url: "https://raydium.io/swap/?inputCurrency=sol&outputCurrency=EiHvfN6HKvY6v6vjaoL1hpCAAxK5QMswDK7SRDM3u5kd&outputSymbol=RASTAMOU&fixed=in&inputMint=sol&outputMint=4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R",
           },
           {
             icon:<img src={socialIcon4} alt="Social Icon" />,
-            url: "https://www.google.com",
+            url: "https://dexscreener.com/?__cf_chl_tk=6UxNBnayvW7qzQDE_iKEFbhF1nlxT5p8d_LpGuHs4Os-1716965713-0.0.1.1-1557",
           },
         ];
       const currentYear = new Date().getFullYear(); 
